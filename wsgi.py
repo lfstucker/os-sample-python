2446dcfa02ea53f7f7646d41b58acb772720ce23
Logan Francis Stucker <39525720+lfstucker@users.noreply.github.com>
2019-11-05 14:48:27 -0600

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World!"
+    return "Hello World!, testing to learn how to redeploy!"
 
 if __name__ == "__main__":
     application.run()
